547838e8296907b23beca37892d8ae34f1906dcc
Rashmika Tharindu <rashmika@agrithmics.lk>
2023-08-10 16:02:10 +0530

    Set connection with mysql db

---
+++ StudioApp/CustomController/BindablePasswordBox.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudioApp.CustomController
+{
+    /// <summary>
+    /// Interaction logic for BindablePasswordBox.xaml
+    /// </summary>
+    public partial class BindablePasswordBox : UserControl
+    {
+        private static DependencyProperty passwordproperty = DependencyProperty.Register("Password", typeof(SecureString), typeof(BindablePasswordBox));
+        public SecureString Password
+        {
+            get { return (SecureString)GetValue(passwordproperty); }
+            set { SetValue(passwordproperty, value); }
+        }
+        public BindablePasswordBox()
+        {
+            InitializeComponent();
+            txtPass.PasswordChanged += OnPasswordChanged;
+        }
+
+        private void OnPasswordChanged(object sender, RoutedEventArgs routedEventArgs)
+        {
+            Password= txtPass.SecurePassword;
+        }
+    }
+}
---
+++ StudioApp/Repositories/UserRepository.cs
@@ -0,0 +1,86 @@
+﻿using StudioApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace StudioApp.Repositories
+{
+    public class UserRepository : RepositoryBase, IUserRepository
+    {
+        public void Add(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AuthenticateUser(NetworkCredential credential)
+        {
+            bool validUser;
+            using (var connection = GetConnection())
+            using (var command = new MySqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM `User` WHERE username=@username AND `password`=@password";
+                command.Parameters.Add("@username", MySqlDbType.VarChar).Value = credential.UserName;
+                command.Parameters.Add("@password", MySqlDbType.VarChar).Value = credential.Password;
+                validUser = command.ExecuteScalar() == null ? false : true;
+            }
+            return validUser;
+        }
+
+        public void Edit(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserModel> GetByAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel GetByUsername(string username)
+        {
+            UserModel user = null;
+            using (var connection = GetConnection())
+            using (var command = new MySqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "select *from [User] where username=@username";
+                command.Parameters.Add("@username", MySqlDbType.VarChar).Value = username;
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        user = new UserModel()
+                        {
+                            Id = reader[0].ToString(),
+                            Username = reader[1].ToString(),
+                            Password = string.Empty,
+                            Name = reader[3].ToString(),
+                            LastName = reader[4].ToString(),
+                            Email = reader[5].ToString(),
+                        };
+                    }
+                }
+            }
+            return user;
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
